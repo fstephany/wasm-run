3a92ce88b43eb88897f452f8035ccd10991a7bba
Francois Stephany <francois@tamere.eu>
2021-10-12 17:58:22 +0200

    WIP Cache busting asset hash

--- examples/testbed/backend/src/main.rs
+++ examples/testbed/backend/src/main.rs
@@ -6,22 +6,22 @@ use rocket::{
 use rocket_dyn_templates::Template;
 use std::collections::HashMap;
 use std::sync::Arc;
+use rocket::serde::json::Value;
+use rocket_dyn_templates::tera::{Context, Result, Tera};
+use rocket_dyn_templates::tera::Filter;
 
-    use rocket::serde::json::Value;
-    use rocket_dyn_templates::tera::{Context, Result, Tera};
-    use rocket_dyn_templates::tera::Filter;
+
 
 
 #[rocket::main]
 pub async fn main() {
     let assets_path = "assets";
-    let brol: HashMap<String, String> = HashMap::new();
-    let assets = Arc::new(brol);
+    let asset_prefix = "".to_string();
 
     rocket::build()
         .attach(Template::custom(move |engines| {
             engines.tera.register_filter("assetify", TeraFilters::AssetsFilter { 
-                asset_store: assets.clone()
+                assets_prefix: asset_prefix.clone()
             });
         }))
         .mount(
@@ -41,13 +41,17 @@ pub mod TeraFilters {
     use rocket_dyn_templates::tera::{Context, Result, Tera};
     use std::sync::Arc;
 
+    /// This filter will convert the relative URL of the assets to the full 
+    /// URL including the content hash if necessary
     pub struct AssetsFilter {
-        pub asset_store: Arc<HashMap<String, String>>,
+        pub assets_prefix: String,
     }
 
     impl rocket_dyn_templates::tera::Filter for AssetsFilter {
         fn filter(&self, value: &Value, _: &HashMap<String, Value>) -> Result<Value> {
-            Ok("This is an asset".into())
+            let full_url = format!("/assets{}/{}", self.assets_prefix, value.to_string());
+
+            Ok(full_url.into())
         }
     }
 } 
--- examples/testbed/run/src/main.rs
+++ examples/testbed/run/src/main.rs
@@ -1,3 +1,73 @@
+use md5::Digest;
+use std::{
+    fs::File,
+    io::Read,
+    path::{Path, PathBuf},
+};
+
+#[derive(Debug)]
+pub struct Asset {
+    pub filename: String,
+    pub path: PathBuf,
+    pub content_hash: String,
+}
+
 fn main() {
     println!("Run");
+
+    // Params:
+    // - assets path
+    // - output path
+    // - config?
+
+    // - go through all the assets
+    // - get their hash
+    // - copy them over the build directory with content hash
+    // - OPTION: Apply optimizer there (e.g., re-encode jpeg?)
+    // - Generate the manifest file
+
+    // Actions:
+    // - prepare
+    // - clean
+    // - info
+    // - watch
+
+    println!("Listing assets");
+    let stylesheets = list_assets("assets/css");
+
+    for style in stylesheets {
+        println!("Style: {:?}", style)
+    }
+}
+
+fn list_assets<P: AsRef<Path>>(path: P) -> Vec<Asset> {
+    // assets/images
+    // assets/css
+    // scss bundles?
+    // wasm bundles?
+    // --> What about SCSS compilation?
+
+    let entries = std::fs::read_dir(path).unwrap();
+
+    entries
+        .map(|entry| {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            let filename = entry.file_name();
+            println!("- {:?}", entry);
+            let mut f = File::open(&path).unwrap();
+
+            let mut buf: Vec<u8> = Vec::new();
+            f.read_to_end(&mut buf).unwrap();
+
+            let digest = md5::Md5::digest(&buf);
+            let content_hash = String::from(digest.to_ascii_lowercase());
+
+            Asset {
+                filename: filename.to_string_lossy().into_owned(),
+                path,
+                content_hash,
+            }
+        })
+        .collect()
 }
